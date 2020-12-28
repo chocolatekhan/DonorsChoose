b438adf4645f92ceac9800865c68291e6c63f2ae
Fida <n.fida6941@gmail.com>
2020-12-28 22:25:15 +0600

    added charity profile

--- app/src/main/java/com/example/donorschoose/CharityProfile.java
+++ app/src/main/java/com/example/donorschoose/CharityProfile.java
@@ -1,14 +1,64 @@
 package com.example.donorschoose;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class CharityProfile extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    String charityName;
+    String charityDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_profile);
+
+        db = FirebaseFirestore.getInstance();
+        retrieveCharityData("BRAC");
+    }
+
+    public void retrieveCharityData(String name)
+    {
+        charityName = name;
+        CollectionReference charities = db.collection("charities");
+        Query q = charities.whereEqualTo("name", charityName);
+
+        q.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task)
+            {
+                if (task.isSuccessful())
+                    if (!task.getResult().isEmpty())
+                        for (QueryDocumentSnapshot document : task.getResult())
+                        {
+                            displayProfile(document);
+                        }
+            }
+        });
+
+    }
+
+    public void displayProfile(QueryDocumentSnapshot document)
+    {
+        charityDescription = document.getString("description");
+
+        TextView nameTextView = (TextView) findViewById(R.id.nameTextView);
+        TextView descriptionTextView = (TextView) findViewById(R.id.descriptionTextView);
+
+        nameTextView.setText(charityName + "");
+        descriptionTextView.setText(charityDescription + "");
     }
 }
