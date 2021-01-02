a13c84c2685f3f2e13128be5dfcdeecfacd0af48
Alvi Khan <aveenalvi@gmail.com>
2021-01-02 21:38:33 +0600

    Added payment gateway

--- app/src/main/java/com/example/donorschoose/Donate.java
+++ app/src/main/java/com/example/donorschoose/Donate.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Donate extends AppCompatActivity {
 
@@ -18,5 +19,12 @@ public class Donate extends AppCompatActivity {
     {
         Intent intent = new Intent(getApplicationContext(), NonMonetaryDonation.class);
         startActivity(intent);
+    }
+
+    public void donate(View view)
+    {
+        int donationAmount = Integer.parseInt(((EditText) findViewById(R.id.donationAmount)).getText().toString());
+        SSLCommerz donation = new SSLCommerz();
+        donation.makePayment(Donate.this, donationAmount);
     }
 }
